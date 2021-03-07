98c0050b16e3b48753c72cb3d9f86eeabd20469c
Nicholas Fehlberg <glenfish@nicholasfehlberg.com>
2021-03-07 18:29:45 +1100

    deleted unused views

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   # tutor_root_path 'tutor#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'restricted', to: 'home#restricted', as: 'restricted'
-  get 'members', to: 'members#index', as: 'members'
+  get 'tutors', to: 'members#index', as: 'tutors'
   get 'students', to: 'student#index', as: 'students'
   get 'students/join/', to: 'student#join', as: 'students_join'
 end
