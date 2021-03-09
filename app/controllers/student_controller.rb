e514e6d11fd741b64146c6c885425729374dc238
Nicholas Fehlberg <glenfish@nicholasfehlberg.com>
2021-03-09 21:29:14 +1100

    added student authentication and searh form

--- app/controllers/student_controller.rb
+++ app/controllers/student_controller.rb
@@ -1,5 +1,19 @@
 class StudentController < ApplicationController
     before_action :authenticate_student!
     def index
+        @tutor = Tutor.first
+    end
+    def search
+        params.permit(:firstname, :lastname, :email, :country)
+        if params[:country]
+            @tutors = Tutor.where(country: params[:country])
+        end
+        if params[:email] != ''
+            @tutors = @tutors.where(email: params[:email])
+        end
+        
+
+        
+        
     end
 end
