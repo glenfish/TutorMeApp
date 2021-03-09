447edbc5d189c821e2204c6cd8e38265872ce9c6
Nicholas Fehlberg <glenfish@nicholasfehlberg.com>
2021-03-09 21:27:14 +1100

    added tutor permitted signup params for model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
     def auth_is_logged_in
         authenticated = false
         if (:authenticate_student! || :authenticate_tutor!)
@@ -6,4 +8,11 @@ class ApplicationController < ActionController::Base
         end
         return authenticated
     end
+
+    protected
+
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :firstname, :lastname, :address_1, :address_2, :postcode, :state, :country])
+    end
+
 end
