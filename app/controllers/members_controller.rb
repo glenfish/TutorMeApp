c0769a32a479949113bca5f1f3a5ea3a069673d0
Nicholas Fehlberg <glenfish@nicholasfehlberg.com>
2021-03-10 13:48:00 +1100

    tutor profile pulling from profiles table added to tutor central page

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,5 +1,7 @@
 class MembersController < ApplicationController
     before_action :authenticate_tutor!
     def index
+        @tutor = Tutor.find_by_id(current_tutor)
+        @profile = @tutor.profile
     end
 end
