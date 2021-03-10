c9cef03dc25b9c7f306efd8a10175a63e179f285
Nicholas Fehlberg <glenfish@nicholasfehlberg.com>
2021-03-10 13:57:01 +1100

    added tutor profile controller

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,7 +1,20 @@
 class MembersController < ApplicationController
     before_action :authenticate_tutor!
+    before_action :get_tutor_and_profile
+
     def index
-        @tutor = Tutor.find_by_id(current_tutor)
-        @profile = @tutor.profile
+        
     end
+
+    def profile_form
+        print params
+    end
+
+    private
+
+    def get_tutor_and_profile
+    @tutor = Tutor.find_by_id(current_tutor)
+    @profile = @tutor.profile
+    end
+
 end
