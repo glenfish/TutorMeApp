57c125a026c325a2c8220127fd2d033b9b448845
Nicholas Fehlberg <glenfish@nicholasfehlberg.com>
2021-03-10 13:30:29 +1100

    created Profile model and 1-1 assoc w/tutors

--- app/models/tutor.rb
+++ app/models/tutor.rb
@@ -1,4 +1,5 @@
 class Tutor < ApplicationRecord
+  has_one :profile
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_07_040146) do
+ActiveRecord::Schema.define(version: 2021_03_10_022402) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "profiles", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.bigint "tutor_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["tutor_id"], name: "index_profiles_on_tutor_id"
+  end
 
   create_table "students", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -51,4 +60,5 @@ ActiveRecord::Schema.define(version: 2021_03_07_040146) do
     t.index ["reset_password_token"], name: "index_tutors_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "profiles", "tutors"
 end
