3d6f72bbe4e3a1fadb4fc882209f0057c842be95
Nicholas Fehlberg <glenfish@nicholasfehlberg.com>
2021-03-11 17:23:33 +1100

    added global var for cloudinary cloud name

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
     before_action :configure_permitted_parameters, if: :devise_controller?
+    $cloud_name = '\"glenfish\"' # this is the cloud name being referenced on cloudinary
 
     def auth_is_logged_in
         authenticated = false
