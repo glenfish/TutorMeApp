6d080ec9b390af289b157f1a46edac27daca2c30
Nicholas Fehlberg <glenfish@nicholasfehlberg.com>
2021-03-13 13:44:08 +1100

    modified payments model

--- db/migrate/20210313023145_create_payments.rb
+++ db/migrate/20210313023145_create_payments.rb
@@ -1,7 +1,7 @@
 class CreatePayments < ActiveRecord::Migration[6.1]
   def change
     create_table :payments do |t|
-      t.decimal :amount, null: false
+      t.integer :amount, null: false
       t.references :booking, null: false, foreign_key: true
 
       t.timestamps
