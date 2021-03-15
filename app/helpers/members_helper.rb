49b5caa862db6c69ad6c0f9c320d9a5a58586914
Nicholas Fehlberg <glenfish@nicholasfehlberg.com>
2021-03-15 17:22:52 +1100

    refactor tutor bookings logic and display

--- app/helpers/members_helper.rb
+++ app/helpers/members_helper.rb
@@ -1,6 +1,6 @@
 module MembersHelper
     def get_tutor_booking_info(booking)
-
+        booking_info = ""
         subject = Subject.find(booking.subject_id)
         subject_title = subject.title.capitalize
         booked_time = booking.time
@@ -14,15 +14,24 @@ module MembersHelper
         student = Student.find(booking.student_id)
         student_name = student.firstname + ' ' + student.lastname
         student_email = student.email
+        booking_info = "<div class='card' style='width: 18rem;'><div class='card-body'><h5 class='card-title'>Booking ID: "
+        booking_info << booking.id.to_s
+        booking_info << "</h5><h6 class='card-subtitle mb-2 text-muted'>"
+        booking_info << booking.created_at.strftime('%d/%m/%Y').to_s
+        booking_info << "</h6><p class='card-text'>Student: "
+        booking_info << student_name
+        booking_info << "<br>Subject: "
+        booking_info << subject_title
+        booking_info << "<br>Time: "
+        booking_info << booked_time>to_s
+        booking_info << "<br>Paid: "
+        booking_info << booking_amount.to_s
+        booking_info << "<br>Student Contact: "
+        booking_info << student_email
+        booking_info << "</p></div></div>"
 
-        booking_info = []
-        booking_info << "Booking ID: #{booking.id}"
-        booking_info << "Date Booked: #{booking.created_at.strftime("%d/%m/%Y")}"
-        booking_info << "Student: #{student_name}"
-        booking_info << "Subject: #{subject_title}"
-        booking_info << "Time booked: #{booked_time}"
-        booking_info << "Charged: #{booking_amount}"
-        booking_info << "Student contact: #{student_email}"
         return booking_info
     end
 end
+
+
